7b3d64b72f44ea1bbe24a49bb83111f75c7c6743
haoyucn <haoyu_cn@hotmail.com>
2021-05-29 15:54:49 -0600

    1

---
+++ db.py
@@ -0,0 +1,31 @@
+import boto3
+import time
+
+dynamodb = boto3.resource('dynamodb')
+
+t1 = time.time()
+
+table = dynamodb.Table('User')
+
+with table.batch_writer() as batch:
+	batch.put_item(
+		Item={
+			'id': 1,
+			'name': 'Hao1'
+		}
+	)
+	batch.put_item(
+		Item={
+			'id': 2,
+			'name': 'Hao2'
+		}
+	)
+	batch.put_item(
+		Item={
+			'id': 1,
+			'name': 'Hao1'
+		}
+	)
+
+print(table.creation_date_time)
+print(time.time() - t1)

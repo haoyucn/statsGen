f68ad5826331d32c562687120623b898540bd673
haoyucn <haoyu_cn@hotmail.com>
2021-05-29 16:07:29 -0600

    1

--- db.py
+++ db.py
@@ -10,22 +10,22 @@ table = dynamodb.Table('User')
 with table.batch_writer() as batch:
 	batch.put_item(
 		Item={
-			'id': 1,
+			'id': '1',
 			'name': 'Hao1'
 		}
 	)
 	batch.put_item(
 		Item={
-			'id': 2,
+			'id': '2',
 			'name': 'Hao2'
 		}
 	)
-	# batch.put_item(
-	# 	Item={
-	# 		'id': 3,
-	# 		'name': 'Hao1'
-	# 	}
-	# )
+	batch.put_item(
+		Item={
+			'id': '3',
+			'name': 'Hao1'
+		}
+	)
 
 print(table.creation_date_time)
 print(time.time() - t1)
